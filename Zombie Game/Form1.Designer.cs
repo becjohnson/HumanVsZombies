468c33131aea87bee190bf8bb0c43d1873bb9114
becjohnson <91417720+becjohnson@users.noreply.github.com>
2021-11-17 15:30:44 -0500

    Final Commit

---
+++ Zombie Game/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace Zombie_Game
+{
+    partial class zombieGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtAmmo = new System.Windows.Forms.Label();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.txtHealth = new System.Windows.Forms.Label();
+            this.healthBar = new System.Windows.Forms.ProgressBar();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.player = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtAmmo
+            // 
+            this.txtAmmo.AutoSize = true;
+            this.txtAmmo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAmmo.ForeColor = System.Drawing.Color.White;
+            this.txtAmmo.Location = new System.Drawing.Point(12, 9);
+            this.txtAmmo.Name = "txtAmmo";
+            this.txtAmmo.Size = new System.Drawing.Size(91, 24);
+            this.txtAmmo.TabIndex = 0;
+            this.txtAmmo.Text = "Ammo : 0";
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.ForeColor = System.Drawing.Color.White;
+            this.txtScore.Location = new System.Drawing.Point(392, 9);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(73, 24);
+            this.txtScore.TabIndex = 1;
+            this.txtScore.Text = "Kills : 0 ";
+            // 
+            // txtHealth
+            // 
+            this.txtHealth.AutoSize = true;
+            this.txtHealth.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHealth.ForeColor = System.Drawing.Color.White;
+            this.txtHealth.Location = new System.Drawing.Point(666, 9);
+            this.txtHealth.Name = "txtHealth";
+            this.txtHealth.Size = new System.Drawing.Size(74, 24);
+            this.txtHealth.TabIndex = 2;
+            this.txtHealth.Text = "Health :";
+            // 
+            // healthBar
+            // 
+            this.healthBar.Location = new System.Drawing.Point(746, 10);
+            this.healthBar.Name = "healthBar";
+            this.healthBar.Size = new System.Drawing.Size(157, 23);
+            this.healthBar.TabIndex = 3;
+            this.healthBar.Value = 100;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // player
+            // 
+            this.player.Image = global::Zombie_Game.Properties.Resources.up;
+            this.player.Location = new System.Drawing.Point(384, 450);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(71, 100);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 4;
+            this.player.TabStop = false;
+            // 
+            // zombieGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(915, 700);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.healthBar);
+            this.Controls.Add(this.txtHealth);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.txtAmmo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "zombieGame";
+            this.Text = "Zombie Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyisDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyisUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtAmmo;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label txtHealth;
+        private System.Windows.Forms.ProgressBar healthBar;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.PictureBox player;
+    }
+}
+
